49c362fb2093d140750cbc2a7f3c3e58b042d200
Makimousse <116353532+Makimousse@users.noreply.github.com>
2022-10-23 19:34:24 +0200

    sex-specific data

--- Code.py
+++ Code.py
@@ -12,4 +12,7 @@ sex = input('Please input your sex (H for male, F for female:')
 age = int(input('Please enter your age (between 2 and 90):'))
 percentage = str(workbook[sex].iloc[age])
 
-print("You have a "+ percentage+"% chance of having some type of cancer")
+print("You have a "+ percentage+"% chance of having some type of cancer.")
+
+if sex == "H":
+    print("If you do have some kind of cancer, there is a 25.4% chance that it is prostate cancer. Prostate cancer has a x% mortality rate at your age")
