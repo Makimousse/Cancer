c772b51af41e2b301e86140d82131244444bc000
Makimousse <116353532+Makimousse@users.noreply.github.com>
2022-10-24 13:19:45 +0200

    gender specific cancer data
    Error on line 17, needs to be fixed

--- Code.py
+++ Code.py
@@ -6,15 +6,13 @@ License = GPL-3
 '''
 import pandas as pd
 url = "https://github.com/Makimousse/Cancer/raw/main/Exel_datasheet.xlsx" 
-df = pd.read_excel(url, usecols = 'A:B:H:I')
+df = pd.read_excel(url, usecols = 'A:B')
 df.head()
 sex = input('Please input your sex (H for male, F for female:')
 age = int(input('Please enter your age (between 2 and 90):'))
 percentage = str(df[sex].iloc[age])
-
-
-
 print("You have a "+ percentage+"% chance of having some type of cancer.")
-
-if sex == "H" and age >= 16:
-    print("If you do have some kind of cancer, there is a 25.4% chance that it is prostate cancer. Prostate cancer has a x% mortality rate at your age")
+if age >= 15:
+    gender_specific = pd.read_excel(url, usecols = 'H:I')
+    gen_spes_percent = str(gender_specific[sex].iloc[age])
+    print("If you do have some kind of cancer, there is a 25.4% chance that it is prostate cancer. Prostate cancer has a"+gen_spes_percent+"% survival chance over 5 years")
