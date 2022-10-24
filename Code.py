4a01a58eb233d64c1f89d6bf9813aaac4d666c92
Makimousse <116353532+Makimousse@users.noreply.github.com>
2022-10-24 19:29:06 +0200

    I dont even know what I did but it works, somehow....

--- Code.py
+++ Code.py
@@ -8,7 +8,7 @@ import pandas as pd
 url = "https://github.com/Makimousse/Cancer/raw/main/Exel_datasheet.xlsx" 
 df = pd.read_excel(url, usecols = 'A:B')
 df.head()
-sex = input('Please input your sex (H for male, F for female:')
+sex = input('Please input your sex (H for male, F for female):')
 age = int(input('Please enter your age (between 2 and 90):'))
 percentage = str(df[sex].iloc[age])
 print("You have a "+ percentage+"% chance of having some type of cancer.")
@@ -18,6 +18,6 @@ if age >= 15:
     gender_specific.head()
     gen_spes_percent = str(gender_specific[sex].iloc[age])
     if sex == "H_canc":
-        print("If you do have some kind of cancer, there is a 25.4% chance that it is prostate cancer. Prostate cancer has a "+gen_spes_percent+"% survival chance over 5 years")
+        print("If you do have some kind of cancer, there is a 25.4% chance that it is prostate cancer. Prostate cancer has a "+gen_spes_percent+"% survival chance over 5 years for your age")
     else:
-        print("if you have some kind of cancer, there is a 30% chance that it is breast cancer. Breast cancer has a "+gen_spes_percent+"% survival chance over 5 years")
+        print("If you have some kind of cancer, there is a 30% chance that it is breast cancer. Breast cancer has a "+gen_spes_percent+"% survival chance over 5 years for your age")
