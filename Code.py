e377285dd92b1a020f68561feda2efd8ccbebe1f
Makimousse <116353532+Makimousse@users.noreply.github.com>
2022-10-24 13:08:28 +0200

    testing

--- Code.py
+++ Code.py
@@ -6,13 +6,15 @@ License = GPL-3
 '''
 import pandas as pd
 url = "https://github.com/Makimousse/Cancer/raw/main/Exel_datasheet.xlsx" 
-workbook = pd.read_excel(url, usecols = 'A:B')
-workbook.head()
+df = pd.read_excel(url, usecols = 'A:B:H:I')
+df.head()
 sex = input('Please input your sex (H for male, F for female:')
 age = int(input('Please enter your age (between 2 and 90):'))
-percentage = str(workbook[sex].iloc[age])
+percentage = str(df[sex].iloc[age])
+
+
 
 print("You have a "+ percentage+"% chance of having some type of cancer.")
 
-if sex == "H":
+if sex == "H" and age >= 16:
     print("If you do have some kind of cancer, there is a 25.4% chance that it is prostate cancer. Prostate cancer has a x% mortality rate at your age")
