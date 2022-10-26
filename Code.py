6a7d822c00b1a49ee71555bec63963bd36724a68
Makimousse <116353532+Makimousse@users.noreply.github.com>
2022-10-26 18:06:11 +0200

    Coded with da bro
    Gotta make the code somewhat understandable

--- Code.py
+++ Code.py
@@ -6,12 +6,18 @@ License = GPL-3
 '''
 import pandas as pd
 while True:
+    # url variable represents the dataset i will be using with variables such person's sex & age.
     url = "https://github.com/Makimousse/Cancer/raw/main/Exel_datasheet.xlsx" 
     df = pd.read_excel(url, usecols = 'A:B')
-    df.head()
+    
+    # df.head()
     sex = input('Please input your sex (H for male, F for female):')
+    
     age = int(input('Please enter your age (between 2 and 90):'))
-    percentage = str(df[sex].iloc[age])
+    
+    percentage = str(df[sex].iloc[age]) # ajdbkabwdck
+    
+    
     print("You have a "+ percentage+"% chance of having some type of cancer.")
     if age >= 15:
         sex = sex+"_canc"
