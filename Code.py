64b6704b694e941ea7de172003325b1382cf51b6
Makimousse <116353532+Makimousse@users.noreply.github.com>
2022-10-28 12:06:26 +0200

    Cleaned up a little

--- Code.py
+++ Code.py
@@ -6,28 +6,27 @@ License = GPL-3
 '''
 import pandas as pd
 while True:
-    # url variable represents the dataset i will be using with variables such person's sex & age.
+    # url variable represents the dataset that will be used with variables such person's sex & age.
     url = "https://github.com/Makimousse/Cancer/raw/main/Exel_datasheet.xlsx" 
     df = pd.read_excel(url, usecols = 'A:B')
-    
-    # df.head()
+
     sex = input('Please input your sex (H for male, F for female):')
-    
     age = int(input('Please enter your age (between 2 and 90):'))
     
-    percentage = str(df[sex].iloc[age]) # ajdbkabwdck
+    percentage = str(df[sex].iloc[age]) # Fetches the percentage data from row "sex" and line "age"
     
-    
-    print("You have a "+ percentage+"% chance of having some type of cancer.")
+    print("You have a "+ percentage+"% chance of currently having some type of cancer.")
+
     if age >= 15:
-        sex = sex+"_canc"
-        gender_specific = pd.read_excel(url, usecols = 'C:D')
-        gender_specific.head()
-        gen_spes_percent = str(gender_specific[sex].iloc[age])
-        if sex == "H_canc":
-            print("If you do have some kind of cancer, there is a 25.4% chance that it is prostate cancer. Prostate cancer has a "+gen_spes_percent+"% survival chance over 5 years for your age")
+        sex2 = sex + "_canc" # The rows for the gender-specific cancer survival data are named H_canc and F_canc
+        df2 = pd.read_excel(url, usecols = 'C:D') # Second dataframe for the gender-specific cancer data, or else python has a stroke for no reason
+        gen_spes_percent = str(df2[sex2].iloc[age]) # Same as "percentage"
+        if sex2 == "H_canc":
+            print("If you do have some kind of cancer, there is a 25.4% chance that it is prostate cancer. Prostate cancer has a "+gen_spes_percent+"% survival chance ")
+            print("over 5 years for your age")
         else:
-            print("If you have some kind of cancer, there is a 30% chance that it is breast cancer. Breast cancer has a "+gen_spes_percent+"% survival chance over 5 years for your age")
-    again = input("Run the program again? (y for yes, n for no):")
+            print("If you have some kind of cancer, there is a 30% chance that it is breast cancer. Breast cancer has a "+gen_spes_percent+"% survival chance ")
+            print("over 5 years for your age")
+    again = input("Run the program again? (y for yes, n for no):") #prompt to run the program again
     if again == "n":
         break
