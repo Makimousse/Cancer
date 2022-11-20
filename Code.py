336b5222d2d4bf70fe6054b067a719ca07b1c1c0
Makimousse <116353532+Makimousse@users.noreply.github.com>
2022-11-20 20:51:35 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -9,9 +9,12 @@ import pandas as pd
 
 url = "https://github.com/Makimousse/Cancer/raw/main/Datasheet1-sex%26age.xlsx" # Url variable represents the dataset that will be used to fetch the data from a person's age and sex.
 df = pd.read_excel(url, usecols = 'A:B') # First dataframe that looks at the sex columns of the excel book
+df_sex = pd.read_excel(url, usecols = 'C:D') # Second dataframe for the gender-specific cancer data, or else python has a stroke for no reason
 
 url2 = "https://github.com/Makimousse/Cancer/raw/main/Datasheet2-country.xlsx" # Second url variable to fetch country-specific cancer percentage rates. I use a different excel sheet to not confuse the two
 df_country = pd.read_excel(url2, usecols = 'A:B')  # Country-specific dataframe
+
+
 
 def input_sex(): # This function will ask the user for his sex and check if the sex is correct with a bool
     sex = input('Please input your sex (M for male, F for female):')
@@ -26,7 +29,6 @@ def input_age(): #This function asks the user for his age, it also manages non-i
         except: # If there is an error, it will restart the function
             input_age()
     return age
-
 def age_checks(age): # This function creates a bool to check if the age is in the correct range
     age_list = [*range(2, 90)]
     age_range_check = age in age_list
@@ -35,28 +37,30 @@ def age_checks(age): # This function creates a bool to check if the age is in th
 def input_country(): # This function asks for the user's country
     country = input("Would you like to know your chance of having cancer in other countries? If yes, please input the country's name, if no, input 'n':") # Asks the user if they would like to know their chances of having cancer in other countries. This country input will be used throughout the code
     return country
+def country_check(country): # This function creates a bool to check if the country is part of the second excel book's
+    country_list_file = open('Country_list.txt', 'r') # Instead of writing in the code a long list of countries, we here fetch this list from an external file
+    file_contents = country_list_file.read() # This interprets the contents of the external file
+    country_list_check = country in file_contents
+    return country_list_check
+
 
 
 
 def base_percentage(sex,age): # Function gives the user his basic chance of having any type of cancer
     percentage = df[sex].iloc[int(age)] # Fetches the percentage data from row "sex" and line "age"
     round_percentage  = '%.4f' % round(percentage, 4) # Rounds the result to 4 digits after decimal point
-    
     return round_percentage, percentage
             
-
 def gen_spes(sex, age): # Finds the user's survival chances for gender-specific cancers (breast/prostate)
     sex2 = sex + "_canc" # The rows for the gender-specific cancer survival data are named H_canc and F_canc, so it is important to rename the sex variable
-
-    df_sex = pd.read_excel(url, usecols = 'C:D') # Second dataframe for the gender-specific cancer data, or else python has a stroke for no reason
-        
+    
     gen_spes_percent = str(df_sex[sex2].iloc[age]) # Same as "percentage", but for the second dataframe
     return gen_spes_percent, sex2
     
 def country_percent(sex,country, percentage): # Finds the user's chances of having cancer in otehr countries
     coef = float(percentage) / float(df[sex].iloc[49]) # Finds the coefficient to determine how chances of having cancer are affected by age, the data from the second excel book is somewhat based on an age of 49 for both men and women, so it is used as the reference point here. This coefficient is necessary since the second excel book doesnt indicate sex and age
     row_find = df_country[df_country['Country'] == country].index.to_list() # This locates the row of the country in the excel book and indexes it to a list (the code doesnt work otherwise, not sure why)
-    
+
     country_rates = [df_country['Rates'].iloc[row_find]] # Fetches the country-specific rates from the excel sheet with the row_find variable found earlier
     country_spes_percent = coef * country_rates[0] # Gets the final country, age and sex specific cancer chances 
     country_spes_percent  = '%.4f' % round(country_spes_percent, 4) # Rounds the percentage to 4 digits after the decimal point 
@@ -68,6 +72,7 @@ def main(): #The main is the 'mother function' and will generate all printed cod
 
 
     # This first "Big section" of the main, it covers inputs and manages errors for them:
+    # These all function on the same principle: the functions asign a bool with the variable (True if the variable is good, False if not), and these lines of code will force the user to try again if the input is invalid
     sex, sex_check_list = input_sex() 
     while sex_check_list == False:  #This first "paragraph" is dedicated for the sex inputs and filtering invalid inputs
         sex, sex_check_list = input_sex()
@@ -78,7 +83,11 @@ def main(): #The main is the 'mother function' and will generate all printed cod
         age = input_age()           
         age_range_check = age_checks(age)
 
-    country = input_country()     #And this one for country inputs
+    country = input_country()     
+    country_list_check = country_check(country)
+    while country_list_check == False:      #And this one for country inputs
+        country = input_country()    
+        country_list_check = country_check(country)
 
 
     # This second big secion takes care of percentage data and printing them:
